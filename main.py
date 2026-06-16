7556c28300a7bac32352dfde32537b2be3773da7
Aditya <adityakrr9973@gmail.com>
2026-06-16 08:06:27 +0530

    changed model for more accurate reasoning

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ import streamlit as st
 api_prod=st.sidebar.text_input(type='password',label='Enter your groq api key')
 
 if api_prod:
-    llm=ChatGroq(model='qwen/qwen3-32b',streaming=True,api_key=api_prod,reasoning_format='hidden')
+    llm=ChatGroq(model='deepseek-r1-distill-llama-70b',streaming=True,api_key=api_prod,reasoning_format='hidden')
     summarization_llm=ChatGroq(model='llama-3.3-70b-versatile',api_key=api_prod)
 else:
-    llm=ChatGroq(model='qwen/qwen3-32b',streaming=True,api_key=api_key_local,reasoning_format='hidden')
+    llm=ChatGroq(model='deepseek-r1-distill-llama-70b',streaming=True,api_key=api_key_local,reasoning_format='hidden')
     summarization_llm=ChatGroq(model='llama-3.3-70b-versatile',api_key=api_key_local)
 
 
